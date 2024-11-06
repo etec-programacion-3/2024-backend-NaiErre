77713f00a6cbdbdebb3130789c9441f555860788
FacuLlamas <f.llamas@alumno.etec.um.edu.ar>
2024-11-06 16:21:13 -0300

    Ultimos cambios

--- models/user.js
+++ models/user.js
@@ -1,27 +1,26 @@
-'use strict';
-const { Model } = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
       // Un usuario tiene un carrito
-      User.hasOne(models.Cart, { foreignKey: 'userId' });
+      User.hasOne(models.Cart, { foreignKey: "userId" });
 
       // Un usuario tiene muchos pedidos
-      User.hasMany(models.Order, { foreignKey: 'userId' });
+      User.hasMany(models.Order, { foreignKey: "userId" });
     }
   }
-  User.init({
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    admin: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false // Por defecto, admin será false
+  User.init(
+    {
+      username: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      role: DataTypes.ENUM("admin", "user"),
     },
-    profilePicture: DataTypes.STRING // Nueva columna para la foto de perfil
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
   return User;
 };
